2795eb2a08abd0931fb0a152412bb7330173a423
Kendall Shearman <kendall.shearman@logicmonitor.com>
2022-10-23 22:01:57 -0500

    update

--- websiteClass/main.py
+++ websiteClass/main.py
@@ -4,9 +4,9 @@ import website
 
 # Add API Credentials
 apiCredentials = {
-    'company': 'lmkendallshearman',
-    'accessid': 'uSzNM8LS94XIy3R47dTy',
-    'accesskey': 'lma__%{D8{^)6~C75t6mt63R-_=63~R24Khx_Gqve]CR7qebc^~)^zy7]w3fuw^ILZjgzODM1M2ItNTcxMS00YzFjLTgwYjYtMWU5NTVkNjU1OTE1L049l8h'
+    'company': '',
+    'accessid': '',
+    'accesskey': ''
 }
 
 #Website Data Payload
